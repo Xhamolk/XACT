de20242957028f1c1397e4e074bb0f3f278786ae
Xhamolk <amilcar.menjivar11@gmail.com>
2012-12-29 17:25:17 -0600

    v0.2.1

--- xact/xk/xact/XActMod.java
+++ xact/xk/xact/XActMod.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 /**
  * XACT adds an electronic crafting table capable of reading recipes encoded into chips.
  */
-@Mod(modid = "xact", name = "XACT Mod", version = "beta-0.2.0")
+@Mod(modid = "xact", name = "XACT Mod", version = "beta-0.2.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true,
 		channels = {"xact_channel"}, packetHandler = PacketHandler.class)
 public class XActMod {
