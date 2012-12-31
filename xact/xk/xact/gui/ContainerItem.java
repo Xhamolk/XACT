5a894cf9005e2cf6194e6275c90977eb989096b3
Xhamolk <amilcar.menjivar11@gmail.com>
2012-12-31 15:17:43 -0600

    Security guarantee for ContainerItem, prevents duplication exploit. Closes #49.

--- xact/xk/xact/gui/ContainerItem.java
+++ xact/xk/xact/gui/ContainerItem.java
@@ -1,13 +1,22 @@
 package xk.xact.gui;
 
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 /**
  * This class provides the methods required by ItemContainer.
  */
 public abstract class ContainerItem extends Container {
+
+	protected final EntityPlayer player;
+
+	public ContainerItem( EntityPlayer player ) {
+		this.player = player;
+	}
 
 	/**
 	 * Whether if the container is in use.
@@ -46,5 +55,61 @@ public abstract class ContainerItem extends Container {
 	 * @see xk.xact.core.ItemContainer#onUpdate(net.minecraft.item.ItemStack, net.minecraft.world.World, net.minecraft.entity.Entity, int, boolean)
 	 */
 	public abstract void onContentsStored(ItemStack itemStack);
+
+	/**
+	 * Whether if <code>slot</code> contains the currently held item.
+	 *
+	 * @param slot the Slot being checked.
+	 * @param player the EntityPlayer that is using the ContainerItem
+	 * @return true if the <code>slot.isSlotInInventory(player.inventory, player.inventory.currentItem)</code> is true.
+	 * @see Slot#isSlotInInventory(net.minecraft.inventory.IInventory, int)
+	 */
+	protected boolean slotContainsHeldItem(Slot slot, EntityPlayer player) {
+		return slot != null && slot.isSlotInInventory( player.inventory, player.inventory.currentItem );
+	}
+
+	/**
+	 * Closes the current Container, and notifies the client to close the GuiContainer.
+	 */
+	protected final void closeGUI() {
+		player.closeScreen();
+	}
+
+	/**
+	 * Basically is the same method as <code>slotClick()</code>.
+	 * This is just a call to <code>super.slotClick()</code>, but you can override with no problem.
+	 *
+	 * If you mod overrides <code>slotClick()</code>, make sure to override this instead.
+	 *
+	 * @param slotID the ID for the Slot clicked. Matches with Slot.slotNumber
+	 * @param buttonPressed the button pressed. For mouse clicks: 0 is left click, 1 is right click.
+	 * @param flag usually, 0 is the regular behaviour, 1 is shift-clicking, 2 is keyboard input.
+	 * @param player the EntityPlayer accessing
+	 * @see Container#slotClick(int, int, int, net.minecraft.entity.player.EntityPlayer)
+	 * @see Slot#slotNumber
+	 *
+	 */
+	protected ItemStack handleSlotClick(int slotID, int buttonPressed, int flag, EntityPlayer player) {
+		return super.slotClick( slotID, buttonPressed, flag, player );
+	}
+
+	// If you mod overrides this, override handleSlotClick instead.
+	@Override
+	public final ItemStack slotClick(int slotID, int buttonPressed, int flag, EntityPlayer player) {
+		Slot slot = 0 <= slotID && slotID < inventorySlots.size() ? (Slot) inventorySlots.get( slotID ) : null;
+
+		// Prevent moving the "held item", for security reasons.
+		if( slot != null && slotContainsHeldItem(slot, player) ) {
+			if( player instanceof EntityPlayerMP ) { // server-side only.
+				closeGUI();
+			}
+			return slot.getStack();
+		}
+		if( flag == 2 && buttonPressed == player.inventory.currentItem ) {
+			return slot != null && slot.getHasStack() ? slot.getStack() : null;
+		}
+
+		return handleSlotClick(slotID, buttonPressed, flag, player);
+	}
 
 }
