705130181c19bfb59a4f91bcae071feb1e4fc0c3
Xhamolk <amilcar.menjivar11@gmail.com>
2013-01-08 18:33:16 -0600

    Buttons framework.

---
+++ xact/xk/xact/gui/button/ButtonDevice.java
@@ -0,0 +1,50 @@
+package xk.xact.gui.button;
+
+import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.GL11;
+import xk.xact.gui.GuiUtils;
+
+
+public class ButtonDevice extends GuiButtonCustom {
+
+	private static final int SIZE = 14;
+
+	private int textureIndex = 0;
+
+	ButtonDevice(int posX, int posY) {
+		super(-1, posX, posY, SIZE, SIZE);
+	}
+
+	@Override
+	public boolean isVisible() {
+		if( !super.isVisible() )
+			return false;
+		if( getMode() == ICustomButtonMode.DeviceModes.CLEAR )
+			return GuiUtils.isShiftKeyPressed();
+		return this.getMode() == ICustomButtonMode.DeviceModes.SAVE;
+	}
+
+	@Override
+	public boolean isModeValid(ICustomButtonMode mode) {
+		return mode instanceof ICustomButtonMode.DeviceModes;
+	}
+
+	@Override
+	protected void drawBackgroundLayer(Minecraft mc, int mouseX, int mouseY) {
+		// Draw button.
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, mc.renderEngine.getTexture("/gfx/xact/gui/buttons_1.png"));
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.drawTexturedModalRect(this.xPosition, this.yPosition, textureIndex * 14, 0, this.width, this.height);
+	}
+
+	@Override
+	protected void drawTooltip(Minecraft mc, int mouseX, int mouseY) {
+		// todo: paint the tool tip with the instructions
+	}
+
+	@Override
+	protected void onModeSet(ICustomButtonMode mode) {
+		this.action = this.textureIndex = ((ICustomButtonMode.DeviceModes)mode).ordinal();
+	}
+
+}
---
+++ xact/xk/xact/gui/button/ButtonItem.java
@@ -0,0 +1,78 @@
+package xk.xact.gui.button;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.item.ItemStack;
+import org.lwjgl.opengl.GL11;
+
+
+public class ButtonItem extends GuiButtonCustom {
+
+	public ItemStack item;
+
+	private boolean isSpecial = false;
+
+	ButtonItem(ItemStack item, int posX, int posY) {
+		super( posX, posY );
+		this.item = item;
+	}
+
+	public void moveButton(int xDiff, int yDiff, int boundX, int boundY) {
+		super.xPosition += xDiff;
+		super.yPosition += yDiff;
+
+		super.drawButton = ( xPosition > 0 && xPosition < boundX )
+				&& ( yPosition > 0 && yPosition < boundY );
+	}
+
+
+	@Override
+	protected void onModeSet(ICustomButtonMode mode) {
+		this.isSpecial = mode == ICustomButtonMode.ItemModes.SPECIAL;
+		super.width = super.height = isSpecial ? 26 : 22;
+	}
+
+	@Override
+	protected void drawBackgroundLayer(Minecraft mc, int mouseX, int mouseY) {
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, mc.renderEngine.getTexture("/gfx/xact/gui/buttons_1.png"));
+		int textureX = isSpecial ? 0 : 26;
+		if( field_82253_i )
+			textureX += 48;
+
+		// Draw button.
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.drawTexturedModalRect(this.xPosition, this.yPosition, textureX, 14, this.width, this.height);
+	}
+
+	@Override
+	protected void drawForegroundLayer(Minecraft mc, int mouseX, int mouseY) {
+		// Draw the item
+		if( item != null )
+			paintItem(mc, item, xPosition,yPosition);
+	}
+
+	@Override
+	protected boolean isModeValid(ICustomButtonMode mode) {
+		return mode instanceof ICustomButtonMode.ItemModes;
+	}
+
+	private void paintItem(Minecraft mc, ItemStack item, int x, int y) {
+		this.zLevel = 100.0F;
+
+		x += (this.width - 16) / 2;
+		y += (this.height - 16) / 2;
+
+		itemRenderer.zLevel = 100.0F;
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.renderEngine, item, x, y);
+
+		// don't paint the item's overlay (stack size and effect)
+		// itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, item, x, y);
+
+		itemRenderer.zLevel = 0.0F;
+		this.zLevel = 0.0F;
+	}
+
+	private static RenderItem itemRenderer = new RenderItem();
+
+}
