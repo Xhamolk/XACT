34ed38fdf49d87240e749e1db2e6c6f2ab196ef6
Xhamolk <amilcar.menjivar11@gmail.com>
2013-01-26 18:02:56 -0600

    Increased the green overlay's alpha layer to 85%

--- xact/xk/xact/gui/client/ChipRenderer.java
+++ xact/xk/xact/gui/client/ChipRenderer.java
@@ -38,7 +38,7 @@ public class ChipRenderer implements IItemRenderer {
 			GL11.glEnable( GL11.GL_LIGHTING );
 			GuiUtils.paintItem( recipe.getResult(), 0, 0, Minecraft.getMinecraft(), itemRender );
 		}
-		GuiUtils.paintEffectOverlay( 0, 0, Minecraft.getMinecraft().renderEngine, itemRender, 0.25f, 0.55f, 0.3f, 0.75f );
+		GuiUtils.paintEffectOverlay( 0, 0, Minecraft.getMinecraft().renderEngine, itemRender, 0.25f, 0.55f, 0.3f, 0.85f );
 
 		drawing = false;
 		GL11.glEnable( GL11.GL_CULL_FACE );
