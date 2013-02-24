76a42af7d7de5757809a2f751c2f11306f7e23e7
Xhamolk <amilcar.menjivar11@gmail.com>
2013-02-23 18:20:33 -0600

    Improved the MixedInventory

--- xact/xk/xact/inventory/MixedInventory.java
+++ xact/xk/xact/inventory/MixedInventory.java
@@ -3,102 +3,137 @@ package xk.xact.inventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 
-public class MixedInventory implements IInventory, IInventoryNBT {
+import java.util.ArrayList;
 
-	/**
-	 * Creates a new Inventory with the specified size and name.
-	 *
-	 * @param size the size of this inventory.
-	 * @param name the name that describes this inventory.
-	 */
-	public MixedInventory(int size, String name) {
-		this.mainInventory = new Inventory( size, name );
+public abstract class MixedInventory implements IInventory {
+
+	private ArrayList<IInventory> inventories;
+	private int totalSize = 0;
+	private String name;
+
+	public MixedInventory(String name) {
+		this.name = name;
+		this.inventories = new ArrayList<IInventory>();
 	}
 
-	private IInventoryNBT mainInventory;
-	private IInventoryNBT secondaryInventory;
 
-
-	public IInventory getSecondaryInventory() {
-		return secondaryInventory;
+	public void addInventory(IInventory inventory) {
+		if( inventory == null )
+			return;
+		inventories.add( inventory );
+		totalSize += inventory.getSizeInventory();
 	}
 
+
+	private boolean isSlotInBounds(int slot) {
+		return slot >= 0 && slot < totalSize;
+	}
+
+	private InvPointer findInventoryBySlot(int slot) {
+		if( isSlotInBounds( slot ) ) {
+			int inventoriesCount = inventories.size();
+			int offSet = 0;
+
+			for( int i = 0; i < inventoriesCount; i++ ) {
+				IInventory inv = inventories.get( i );
+				int size = inv.getSizeInventory();
+				if( slot - offSet < size ) {
+					return new InvPointer( i, offSet );
+				}
+				offSet += size;
+			}
+		}
+		return new InvPointer( -1, 0 );
+	}
 
 	///////////////
-	///// NBT
-
-	@Override
-	public void readFromNBT(NBTTagCompound compound) {
-		mainInventory.readFromNBT( compound );
-		secondaryInventory.readFromNBT( compound );
-	}
-
-	@Override
-	public void writeToNBT(NBTTagCompound compound) {
-		mainInventory.writeToNBT( compound );
-		secondaryInventory.writeToNBT( compound );
-	}
-
-
-	///////////////
-	///// IInventory (the main, public inventory)
-
+	///// IInventory
 
 	@Override
 	public int getSizeInventory() {
-		return mainInventory.getSizeInventory();
+		return totalSize;
 	}
 
 	@Override
 	public ItemStack getStackInSlot(int slot) {
-		return mainInventory.getStackInSlot( slot );
+		InvPointer pointer = findInventoryBySlot( slot );
+		IInventory inventory = pointer.getInventory();
+		if( inventory != null ) {
+			inventory.getStackInSlot( slot - pointer.offset );
+		}
+		return null;
 	}
 
 	@Override
 	public ItemStack decrStackSize(int slot, int amount) {
-		return mainInventory.decrStackSize( slot, amount );
+		InvPointer pointer = findInventoryBySlot( slot );
+		IInventory inventory = pointer.getInventory();
+		if( inventory != null ) {
+			inventory.decrStackSize( slot - pointer.offset, amount );
+		}
+		return null;
 	}
 
 	@Override
 	public ItemStack getStackInSlotOnClosing(int slot) {
-		return mainInventory.getStackInSlotOnClosing( slot );
+		InvPointer pointer = findInventoryBySlot( slot );
+		IInventory inventory = pointer.getInventory();
+		if( inventory != null ) {
+			inventory.getStackInSlotOnClosing( slot - pointer.offset );
+		}
+		return null;
 	}
 
 	@Override
 	public void setInventorySlotContents(int slot, ItemStack itemStack) {
-		mainInventory.setInventorySlotContents( slot, itemStack );
+		InvPointer pointer = findInventoryBySlot( slot );
+		IInventory inventory = pointer.getInventory();
+		if( inventory != null ) {
+			inventory.setInventorySlotContents( slot - pointer.offset, itemStack );
+		}
 	}
 
 	@Override
 	public String getInvName() {
-		return mainInventory.getInvName();
+		return "MixedInventory:" + name;
 	}
 
 	@Override
 	public int getInventoryStackLimit() {
-		return mainInventory.getInventoryStackLimit();
+		return 64;
 	}
 
 	@Override
-	public void onInventoryChanged() {
-		mainInventory.onInventoryChanged();
-	}
+	public abstract void onInventoryChanged();
 
 	@Override
 	public boolean isUseableByPlayer(EntityPlayer player) {
-		return mainInventory.isUseableByPlayer( player );
+		return true;
 	}
 
 	@Override
 	public void openChest() {
-		mainInventory.openChest();
 	}
 
 	@Override
 	public void closeChest() {
-		mainInventory.closeChest();
+	}
+
+	private class InvPointer {
+		final int index;
+		final int offset;
+
+		InvPointer(int index, int offset) {
+			this.index = index;
+			this.offset = offset;
+		}
+
+		IInventory getInventory() {
+			if( index == -1 )
+				return null;
+			return inventories.get( index );
+		}
 	}
 
 }
