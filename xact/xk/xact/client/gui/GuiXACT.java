dda426d4cfe75272929cc63930f86036f0bbbdd9
Xhamolk <amilcar.menjivar11@gmail.com>
2013-06-17 11:40:07 -0600

    Code clean up - Will develop new features on separate branches. Got rid of dynamic slot painting

--- xact/xk/xact/client/gui/GuiXACT.java
+++ xact/xk/xact/client/gui/GuiXACT.java
@@ -3,11 +3,8 @@ package xk.xact.client.gui;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
-import xk.xact.client.GuiUtils;
-import xk.xact.client.gui.hooks.DynamicSlotPaintHook;
 
 /**
  * Base class for all the Gui components in XACT
@@ -16,14 +13,11 @@ import xk.xact.client.gui.hooks.DynamicSlotPaintHook;
  */
 public abstract class GuiXACT extends GuiContainer {
 
-	public final boolean hasDynamicPaintingHook;
-
 	protected int mouseX = 0;
 	protected int mouseY = 0;
 
 	public GuiXACT(Container container) {
 		super( container );
-		this.hasDynamicPaintingHook = this instanceof DynamicSlotPaintHook;
 	}
 
 	@Override
@@ -78,41 +72,6 @@ public abstract class GuiXACT extends GuiContainer {
 
 	protected void drawToolTip() {}
 
-	// ------------------------- Draw Slot --------------------------
-
-	@Override
-	protected void drawSlotInventory(Slot slot) {
-		boolean delegateSuperCall = true;
-		int underlayColor = -1;
-		int overlayColor = 1;
-		ItemStack itemToPaint = null;
-
-		if( hasDynamicPaintingHook ) {
-			DynamicSlotPaintHook dyn = (DynamicSlotPaintHook) this;
-			boolean mouseOver = isMouseOverSlot( slot, mouseX, mouseY );
-
-			delegateSuperCall = dyn.shouldDelegateSuperCall( slot );
-			underlayColor = dyn.getSlotUnderlayColor( slot, mouseOver );
-			overlayColor = dyn.getSlotOverlayColor( slot, mouseOver );
-			itemToPaint = dyn.getStackToPaintInSlot( slot, mouseOver );
-		}
-
-		int dim = getSlotDimensions( slot );
-		if( underlayColor != -1 ) { // Paint the slot's underlay color
-			GuiUtils.paintOverlay( slot.xDisplayPosition, slot.yDisplayPosition, dim, underlayColor );
-		}
-
-		if( delegateSuperCall ) { // let the super class handle this.
-			super.drawSlotInventory( slot );
-
-		} else if( itemToPaint != null ) { // paint the item
-			GuiUtils.paintItem( itemToPaint, slot.xDisplayPosition, slot.yDisplayPosition, this.mc, itemRenderer );
-		}
-
-		if( overlayColor != -1 ) { // Paint the slot's overlay color
-			GuiUtils.paintOverlay( slot.xDisplayPosition, slot.yDisplayPosition, dim, overlayColor );
-		}
-	}
 
 	protected int getSlotDimensions(Slot slot) {
 		return 16;
